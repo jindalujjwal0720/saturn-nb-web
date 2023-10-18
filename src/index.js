534f81466048db98de7c36c662da19a7ea4dc884
Ujjwal Jindal <jindalujjwal0720@gmail.com>
2023-10-18 18:34:04 +0530

    re installed dependencies

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import "./variables.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { HashRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./state/redux/store";
@@ -18,8 +17,3 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
